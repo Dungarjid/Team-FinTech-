9fa024c0bd59f9d68a0f80a8b119b98714be5fd5
Dungarjid <mangadun973@gmail.com>
2022-11-22 18:57:02 +0800

    uploading a website

---
+++ exercises/functions.js
@@ -0,0 +1,10 @@
+function multiply(a, b){
+    return a*b;
+}
+function name(params) {
+    let a = "3";
+    let b = "7";
+    parseInt(a, a);
+    parseInt(b, b);
+    return a + b; 
+}
---
+++ exercises/ifelse.js
@@ -0,0 +1,18 @@
+let D; 
+D=parseInt(7)
+if (D>0){
+    document.write("If your discrimiant is "+D+", your equation has two solutions");
+}
+else if(D==0){
+    document.write("your equation has one solution");
+}
+else{
+    document.write("your equation has no augment solution");
+}
+let f = 6; 
+if (f%2==0) {
+    document.write(f+"is an even number");
+}
+else{
+    document.write(f+"is an odd number");
+} 
